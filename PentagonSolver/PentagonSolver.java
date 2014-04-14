9fa5ccd7e0f86cdbc33fdf8727429a33a6ef96a8
waverill7 <waverill@lion.lmu.edu>
2014-04-13 23:52:24 -0700

    Update PentagonSolver.java

--- PentagonSolver/PentagonSolver.java
+++ PentagonSolver/PentagonSolver.java
@@ -9,28 +9,28 @@ public class PentagonSolver {
     }
 
     public static void solve () {
-    	int n = 10;
+        int n = 10;
         Permutation pentagon = new Permutation(n);
         int numberOfSides = 5;
         int[] sideSums = new int[numberOfSides];
         boolean solution = true;
         for (int permutation = 0; permutation < Permutation.totalPermutations(n); permutation++) {
-        	for (int side = 0; side < numberOfSides; side++) {
-        		sideSums[side] = 0;
-        	}
+            for (int side = 0; side < numberOfSides; side++) {
+                sideSums[side] = 0;
+            }
             for (int side = 0; side < numberOfSides; side++) {
                 for (int pocket = (side * 2); pocket <= ((side + 1) * 2); pocket++) {
                     sideSums[side] += pentagon.getElement(pocket % n);
                 }
                 if (sideSums[side] != sideSums[0]) {
-                	solution = false;
-                	break;
+                    solution = false;
+                    break;
                 }
             }
             if (solution) {
-            	System.out.println(pentagon.toString());
+                System.out.println(pentagon.toString());
             } else {
-            	solution = true;
+                solution = true;
             }
             pentagon.advance();
         }
